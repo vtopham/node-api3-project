16a4269482e6d9efe15b793b7356626b14fdf8f7
Victoria  Topham <topham.victoria@gmail.com>
2020-05-06 10:54:28 -0700

    validate user id middleware added

--- server.js
+++ server.js
@@ -5,17 +5,36 @@ const server = express();
 server.use(express.json())
 
 server.use(logger)
-server.get('/', (req, res) => {
+server.use(validateUserId("1"))
+
+server.get('/',(req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
+//logger
 function logger(req, res, next) {
-  res.status(200).json({method: req.method, url: req.url, time: new Date().toISOString()})
-  console.log(req)
-
+  console.log({method: req.method, url: req.url, time: new Date().toISOString()})
   next();
+}
+
+//check the userid
+function validateUserId(userId) {
+
+  return function(req, res, next) {
+    if(!req.headers.id) {
+      res.status(400).json({message: "please include a user id in your header"})
+    } else {
+      const id = req.headers.id
+      if (id === userId) {
+        next();
+      } else {
+        res.status(400).json({message: "invalid user id"})
+      }
+
+    }
+  }
 }
 
 
