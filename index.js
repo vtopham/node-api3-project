5c5f6688cae738b240e4905b846e8f1030f74c37
Victoria  Topham <topham.victoria@gmail.com>
2020-05-06 10:48:03 -0700

    logger middleware added

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code away!
+
+const server = require('./server.js')
+
+server.listen(4000, _ => {
+    console.log("Listening on 4000")
+})
--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json())
+
+server.use(logger)
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  res.status(200).json({method: req.method, url: req.url, time: new Date().toISOString()})
+  console.log(req)
+
+  next();
+}
+
 
 module.exports = server;
