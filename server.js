5787a5b8421a72866edc63751ac406548e4f8e32
Victoria  Topham <topham.victoria@gmail.com>
2020-05-06 11:00:25 -0700

    validate user and validate post middleware added

--- server.js
+++ server.js
@@ -32,10 +32,33 @@ function validateUserId(userId) {
       } else {
         res.status(400).json({message: "invalid user id"})
       }
-
     }
   }
+
 }
 
+//validates the user , to be used on a request to create a new user
+function validateUser(req, res, next) {
+  if(!req.body) {
+    res.status(400).json({message: "missing user data"})
+  } else if (!req.body.name) {
+    res.status(400).json({message: "missing required name field"})
+  } else {
+    next()
+  }
+
+}
+
+
+//validates the body on a request to make a new post
+function validatePost(req, res, next) {
+  if(!req.body) {
+    res.status(400).json({message: "missing post data"})
+  } else if (!req.body.text) {
+    res.status(400).json({message: "missing required text field"})
+  } else {
+    next()
+  }
+}
 
 module.exports = server;
