61fa138ea0b38481460fefac209fce480a311a05
Victoria  Topham <topham.victoria@gmail.com>
2020-05-06 11:19:58 -0700

    endpoint get /users created successfully

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ server.use(express.json())
 
 
 server.use(logger)
-server.use(validateUserId)
 
 server.get('/',(req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -22,52 +21,52 @@ function logger(req, res, next) {
 }
 
 //check the userid
-function validateUserId(req, res, next) {
+// function validateUserId(req, res, next) {
  
-  if(!req.headers.id) {
-    res.status(400).json({message: "please include a user id in your header"})
-  } else {
-    const id = req.headers.id
-    //check to see if it's in the database
+//   // if(!req.headers.id) {
+//   //   res.status(400).json({message: "please include a user id in your header"})
+//   // } else {
+//   //   const id = req.headers.id
+//   //   //check to see if it's in the database
 
-    userDb.getById(id).then(response => {
-      if(response) {
-        next();
-      } else {
-        res.status(400).json({message: "invalid user id"})
-      }
-      next()
-    }).catch(_ => {
-      res.status(500).json({message: "error validating userID"})
-    })
+//   //   userDb.getById(id).then(response => {
+//   //     if(response) {
+//   //       next();
+//   //     } else {
+//   //       res.status(400).json({message: "invalid user id"})
+//   //     }
+//   //     next()
+//   //   }).catch(_ => {
+//   //     res.status(500).json({message: "error validating userID"})
+//   //   })
 
-  }
+//   // }
   
 
-}
+// }
 
 //validates the user , to be used on a request to create a new user
-function validateUser(req, res, next) {
-  if(!req.body) {
-    res.status(400).json({message: "missing user data"})
-  } else if (!req.body.name) {
-    res.status(400).json({message: "missing required name field"})
-  } else {
-    next()
-  }
+// function validateUser(req, res, next) {
+//   if(!req.body) {
+//     res.status(400).json({message: "missing user data"})
+//   } else if (!req.body.name) {
+//     res.status(400).json({message: "missing required name field"})
+//   } else {
+//     next()
+//   }
 
-}
+// }
 
 //validates the body on a request to make a new post
-function validatePost(req, res, next) {
-  if(!req.body) {
-    res.status(400).json({message: "missing post data"})
-  } else if (!req.body.text) {
-    res.status(400).json({message: "missing required text field"})
-  } else {
-    next()
-  }
-}
+// function validatePost(req, res, next) {
+//   if(!req.body) {
+//     res.status(400).json({message: "missing post data"})
+//   } else if (!req.body.text) {
+//     res.status(400).json({message: "missing required text field"})
+//   } else {
+//     next()
+//   }
+// }
 
 
 
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,17 +1,28 @@
 const express = require('express');
+const userDb = require('./userDb.js')
+const postDb = require('../posts/postDb')
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+
+
+//add a new user
+router.post('/', validateUser, (req, res) => {
+  
 });
 
+//add a new post
 router.post('/:id/posts', (req, res) => {
   // do your magic!
 });
 
+//get users
 router.get('/', (req, res) => {
-  // do your magic!
+  userDb.get().then(users => {
+    res.status(200).json({data: users})
+  }).catch(_ => {
+    res.status(500).json({message: "error retrieving list of users"})
+  })
 });
 
 router.get('/:id', (req, res) => {
@@ -32,16 +43,54 @@ router.put('/:id', (req, res) => {
 
 //custom middleware
 
+//run this when you want to validate that the userId in the header exists
 function validateUserId(req, res, next) {
-  // do your magic!
+  //validate that there's a header.id
+  if(!req.headers.id) {
+    res.status(400).json({message: "please include a user id in your header"})
+  } else {
+    const id = req.headers.id
+    //check to see if it's in the database
+
+    userDb.getById(id).then(response => {
+      if(response) {
+        //if it's in the database then move on
+        next();
+      } else {
+        //if it's not in the database, throw an error
+        res.status(400).json({message: "invalid user id"})
+      }
+      next()
+    }).catch(_ => {
+      res.status(500).json({message: "error validating userID"})
+    })
+
+  }
+  
 }
 
+//run this when you want to make sure that there's a user name in the body, to be used when you want to add a new user
 function validateUser(req, res, next) {
   // do your magic!
+  if(!req.body) {
+    res.status(400).json({message: "missing user data"})
+  } else if (!req.body.name) {
+    res.status(400).json({message: "missing required name field"})
+  } else {
+    next()
+  }
 }
 
+//run this when you want to make sure that there's text in the post body, to be used when you want to add a new post
 function validatePost(req, res, next) {
   // do your magic!
+  if(!req.body) {
+    res.status(400).json({message: "missing post data"})
+  } else if (!req.body.text) {
+    res.status(400).json({message: "missing required text field"})
+  } else {
+    next()
+  }
 }
 
 module.exports = router;
